33f1d95438e27690c72c99d7cee18cd512a89b99
Aleksey Pleshkov <alekseypleshkov@icloud.com>
2025-01-16 21:45:16 +0200

    Updates swift package

--- Package.swift
+++ Package.swift
@@ -6,9 +6,14 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "GenerateStruct",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GenerateStruct",
             targets: ["GenerateStruct"]
@@ -22,9 +27,6 @@ let package = Package(
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "GenerateStructMacros",
             dependencies: [
@@ -32,12 +34,7 @@ let package = Package(
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "GenerateStruct", dependencies: ["GenerateStructMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
         .executableTarget(name: "GenerateStructClient", dependencies: ["GenerateStruct"]),
-
     ]
 )
